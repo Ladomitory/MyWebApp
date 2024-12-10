7e1257a1b5a57a86ace64ab05e1295eb50837817
Radomir <radomirholmogorov@yandex.ru>
2024-12-10 17:31:25 +0700

    Version 3.0 (small)

--- src/main/java/app/servlets/RegistrationServlet.java
+++ src/main/java/app/servlets/RegistrationServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Date;
 import java.time.LocalDate;
 
 public class RegistrationServlet extends HttpServlet {
